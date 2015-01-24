e4b1aec7194244cf423e5d762b794d7e55e062f6
hndfwxf <hndfwxf@163.com>
2015-01-24 21:08:52 +0800

    sa14226430

--- client.c
+++ client.c
@@ -1,261 +1,134 @@
 #include <stdio.h>
-
 #include <string.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <unistd.h>
-
 #include <errno.h>
-
 #include <time.h>
-
 #include <fcntl.h>
-
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
 
-
-
-#define    MAXLINE        1024*16  /*ip packet size*/
-
-#define    SERVER_PORT    8888    
-
-#define    OFFSET         1024*1024*128      /*offset in file pointer */
-
+#define    MAXLINE        1024*16  /*数据包大小*/
+#define    SERVER_PORT    6666    /*端口号*/
+#define    OFFSET         1024*1024*128      /*文件指针偏移量 */
 #define    FILE_NAME_MAX_SIZE 512    
+#define    THREAD_NUM       8             /*线程数*/
 
-#define    THREAD_NUM       8             /*number of thread*/
-
-
-
-char  file_name[FILE_NAME_MAX_SIZE]="test";  /*filename on server*/
-
-void* connectToServer(void* arg);   /*thread function*/
-
-
+char  file_name[FILE_NAME_MAX_SIZE]="test";  /*设置默认传输的文件名*/
+void* connectToServer(void* arg);   /*声明连接服务端函数*/
 
 int main(int argc,char **argv)
-
 {
-
     struct sockaddr_in     serv_addr;
-
     time_t                 t_start,t_end;
-
     pthread_t              tempthread_id[THREAD_NUM]; 
-
     int                    i; 
 
-
-
-    /*initialize server address*/
-
+    /*初始化服务端地址*/
     memset(&serv_addr, 0, sizeof(serv_addr));
-
     serv_addr.sin_family = AF_INET;
-
     serv_addr.sin_port = htons(SERVER_PORT);
-
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 
-
-     /* start count time*/
-
+     /* 开始计时*/
      t_start=time(NULL);   
-
      for (i=0; i<THREAD_NUM; i++) 
-
      {   
-
-          
-
-         while (pthread_create (&tempthread_id[i], NULL, (void *)connectToServer, (void *)&serv_addr) != 0)     //create pthread
-
+         while (pthread_create (&tempthread_id[i], NULL, (void *)connectToServer, (void *)&serv_addr) != 0)     //创建线程
          {    
-
-               printf ("create thread error: %s.\n\n", strerror(errno));          
-
+               printf ("线程创建失败: %s.\n\n", strerror(errno));          
          } 
 
      } 
 
-
-
-    /*wait for pthread to exit*/
+    /*等待线程退出*/
 
     for (i=0; i<THREAD_NUM; i++)  
-
     {
-
-	pthread_join(tempthread_id[i], NULL);
-
-	printf(" thread id = %d exit!\n", tempthread_id[i]);
-
+		pthread_join(tempthread_id[i], NULL);
+		printf(" 线程id = %d 退出!\n", tempthread_id[i]);
     } 
 
-
-
-    /*finish count time*/
-
+    /*结束计时*/
     t_end=time(NULL);  
-
     printf("共用时%.0fs\n",difftime(t_end,t_start));
-
     printf("%s 传输成功!\n ", file_name);
-
     return 0;
-
 }
 
 
-
-
-
 void *connectToServer(void *arg)
-
 {
-
-    char                   buf[MAXLINE];  /*received file buffer*/
-
+    char                   buf[MAXLINE];  /*文件缓冲区*/
     int                    sock_id;
-
     int                    recv_len;
-
     int                    write_len;
-
     int                    send_len;
-
     FILE                   *fp;
-
     int                    i_ret;
-
-    int                    threadIndex;         /*server thread index to position write-pointer*/
-
-//   char                   client_file_name[FILE_NAME_MAX_SIZE] = "recvfile.txt";   //received filename 
-
-    //int             	   blockCount = 0;        
-
- 
+    int                    threadIndex;         /*服务端线程索引*/ 
 
     if((sock_id = socket (AF_INET, SOCK_STREAM, 0)) == 0)  
-
     {   
-
         printf ("端口错误: %s.\n\n", strerror(errno));  
-
     }   
 
-
-
-     /* open received file*/   
-
+     /* 准备写入文件*/   
     fp = fopen(file_name, "w");    
-
     if(NULL == fp)    
-
-    {      
-
-	printf("文件\t%s 无法写入\n", file_name);       
-
-	exit(1);    
-
+    {   
+		printf("文件\t%s 无法写入\n", file_name);       
+		exit(1);    
     } 
 
-    /*connect request*/
-
+    /*请求连接*/
     i_ret = connect(sock_id, (struct sockaddr*)arg, sizeof(struct sockaddr));
-
     if (-1 == i_ret) 
-
     {
-
         printf("连接端口失败\n");
-
-	return NULL;
-
+		return NULL;
     }
 
-    /*send file name to server*/
-
+    /*发送文件名给服务器*/
     if((send_len = send(sock_id, file_name, sizeof(file_name), 0)) < sizeof(file_name))
-
     {
-
-	printf("发送文件名失败\n");
-
-	exit(1); 
-
+		printf("发送文件名失败\n");
+		exit(1); 
     }
 
-    /*receive threadIndex from server*/
-
+    /*从服务端接收线程索引*/
     if(recv(sock_id, buf, MAXLINE, 0) < 0)  
-
     {
-
-	printf("线程 id = %d, 接收线程id失败\n", pthread_self());
-
-	exit(1); 
-
+		printf("线程 id = %d, 接收线程id失败\n", pthread_self());
+		exit(1); 
     }
-
-
-
+	
     threadIndex = atoi(buf);
-
-    /*position file write-pointer*/
-
+    /*指定写入位置*/
     fseek(fp, OFFSET*(threadIndex-1), 0);
-
-
-
     bzero(buf, MAXLINE);
 
     while (recv_len = recv(sock_id, buf, MAXLINE, 0)) 
-
     {   
-
         if(recv_len <= 0) 
-
-	{
-
+		{
             printf("接收服务端数据失败!\n");
-
             break;
-
         }
-
-        write_len = fwrite(buf, sizeof(char), recv_len, fp);    /*write buffer to file*/
-
+        write_len = fwrite(buf, sizeof(char), recv_len, fp);    /*把缓冲区数据写入文件*/
         if (write_len < recv_len ) 
-
-	{
-
+		{
             printf("写文件失败\n");
-
             break;
-
         }
-
         bzero(buf, MAXLINE);
-
     }
-
-     
-
     fclose(fp);
-
     close(sock_id);
-
     pthread_exit(NULL);
 
 }
--- server.c
+++ server.c
@@ -1,274 +1,145 @@
 #include <stdio.h>
-
 #include <string.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <unistd.h>
-
 #include <sys/types.h>
-
 #include <pthread.h>
-
 #include <errno.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
 
-
-
-#define    MAXLINE         1024*16   /*ip packet size*/
-
-#define    SERVER_PORT     8888  
-
-#define    OFFSET         1024*1024*128      /*offset in file pointer */
-
+#define    MAXLINE         1024*16   /*数据包大小*/
+#define    SERVER_PORT     6666  /*端口号*/
+#define    OFFSET         1024*1024*128      /*文件指针偏移量 */
 #define    FILE_NAME_MAX_SIZE 512 
 
+void fileTransfer(void *arg);    /*线程*/
+static  int  threadCount = 0;  /*线程计数器*/
 
-
-void fileTransfer(void *arg);    /*function that every thread execute*/
-
-
-
-static  int  threadCount = 0;  //public resources used to count thread created
-
-pthread_mutex_t alock = PTHREAD_MUTEX_INITIALIZER;  //thread lock
-
+pthread_mutex_t alock = PTHREAD_MUTEX_INITIALIZER;  /*线程锁*/
 char  file_name[FILE_NAME_MAX_SIZE];   
 
-
-
 int main(int argc,char **argv) 
-
 {
-
     struct sockaddr_in    serv_addr;
-
     struct sockaddr_in    clie_addr;
-
     int                   sock_id;
-
     int                   clie_addr_len;
-
     pthread_t             thread_id;
-
     int                   *link_id; 
 
-
-
     if ((sock_id = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-
     {
-
         perror("创建socket失败\n");
-
         exit(0);
-
     }
 
-    /*initialize server address*/
-
+    /*初始化服务端*/
     memset(&serv_addr, 0, sizeof(serv_addr));
-
     serv_addr.sin_family = AF_INET;
-
     serv_addr.sin_port = htons(SERVER_PORT);
-
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    /*bind socket with server address*/
-
+	
+    /*绑定端口*/
     if (bind(sock_id, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-
     {
-
         perror("端口绑定失败\n");
-
         exit(0);
-
     }
 
     if (-1 == listen(sock_id, 10)) 
-
     {
-
         perror("端口监听失败\n");
-
         exit(0);
-
     }
 
-    /*waiting for connect request*/
-
+    /*等待客户端连接*/
     while (1) 
-
     {
-
         clie_addr_len = sizeof(clie_addr);
-
-	link_id = (int *)malloc(sizeof(int));
-
-	/*create link socket when connect request arrived*/
-
+		link_id = (int *)malloc(sizeof(int));
+		
+		/*客户端请求到达时建立连接*/
         *link_id = accept(sock_id, (struct sockaddr *)&clie_addr, &clie_addr_len);  
-
     	if (-1 == *link_id) 
-
-	{
-
+		{
             perror("接收端口失败\n");
-
             continue;
-
         }
 
-	/*create thread to deal with every connect request*/
-
-	while (pthread_create (&thread_id, NULL, (void *)fileTransfer, (void *)link_id) != 0)   
-
+		/*创建线程处理客户端请求*/
+		while (pthread_create (&thread_id, NULL, (void *)fileTransfer, (void *)link_id) != 0)   
         {    
-
-             printf ("线程创建失败: %s.\n\n", strerror(errno));
-
-	     break;          
-
+            printf ("线程创建失败: %s.\n\n", strerror(errno));
+			break;          
         }
-
     }
-
-
-
     printf("%s 传输成功!\n ", file_name);
-
     close(sock_id); 
-
     return 0;
-
 }
-
-
 
 
 
 void fileTransfer(void *arg)
-
 { 
-
 	int                    *link_id;
-
-        FILE                   *fp;
-
+    FILE                   *fp;
    	int                    read_len;
-
    	int                    send_len;
-
-    	int                    recv_len;
-
-        char                   buf[MAXLINE];
-
+	int                    recv_len;
+    char                   buf[MAXLINE];
 	int                    threadIndex;
 
-
-
 	link_id = (int *)malloc(sizeof(int));
-
 	*link_id = *((int*)arg);
 
-
-
-	/*action on public on resources*/
-
+	/*互斥锁*/
 	pthread_mutex_lock(&alock);
-
 	threadCount++;
-
 	threadIndex = threadCount;
-
 	pthread_mutex_unlock(&alock);
 
-
-
-	/*receive filename to transfer*/
-
+	/*接收客户端请求的文件名*/
 	if((recv_len = recv(*link_id, file_name, FILE_NAME_MAX_SIZE, 0)) == -1) 
-
 	{
-
 		printf("文件名错误, error = %d\n", errno);
-
 		exit(1);
-
 	}
 
-	/*open file to read*/
-
+	/*打开文件*/
 	if ((fp = fopen(file_name, "r")) == NULL)
-
 	{
-
 		perror("打开文件失败\n");
-
 		exit(0);
-
 	}
 
-	/*positon the read-pointer*/
-
+	/*定位读指针*/
 	fseek(fp, OFFSET*(threadIndex-1), 0);
-
-
-
-	/*send thread index to client*/
-
+	
+	/*发送线程索引给客户端*/
 	sprintf(buf, "%d", threadIndex);
-
 	if((send_len = send(*link_id, buf, sizeof(buf), 0)) < sizeof(buf))
-
 	{
-
 		perror("发送线程id失败\n");
-
 		exit(0);
-
 	}
-
 	bzero(buf, MAXLINE);
-
-	while ((read_len = fread(buf, sizeof(char), MAXLINE, fp)) >0)  /*read content to buf from file */
-
+	while ((read_len = fread(buf, sizeof(char), MAXLINE, fp)) >0)  /*读数据至缓冲区 */
 	{
-
-		send_len = send(*link_id, buf, read_len, 0);   /*send buf content*/
-
+		send_len = send(*link_id, buf, read_len, 0);   /*发送数据*/
 		if ( send_len < read_len ) 
-
 		{
-
 			perror("文件发送失败\n");
-
 			exit(0);
-
 		}
-
-	        bzero(buf, MAXLINE);
-
-		if(ftell(fp) >= OFFSET*threadIndex)    /*when read-pointer arrive the boundary, exit and terminated the thread*/
-
+	    bzero(buf, MAXLINE);
+		if(ftell(fp) >= OFFSET*threadIndex)    /*读至边界退出线程*/
 			break;
-
 	}
-
-        fclose(fp);
-
-        close(*link_id);
-
+    fclose(fp);
+    close(*link_id);
 	pthread_exit(NULL);
-
 }
-
